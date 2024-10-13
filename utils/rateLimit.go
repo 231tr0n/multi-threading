4070889cf3d834f0ff467b1000dad8cd8839c8b8
231tr0n <zeltronsrikar@gmail.com>
2024-10-13 21:17:01 +0000

    test

--- utils/rateLimit.go
+++ utils/rateLimit.go
@@ -14,29 +14,29 @@ import (
 	"time"
 )
 
-var url string
-var requests int
-var debug bool
+var url *string
+var requests *int
+var debug *bool
 
 func init() {
 	log.SetFlags(0)
 	log.SetPrefix("")
 
-	url = *flag.String("host", "", "Host to request")
-	requests = *flag.Int("requests", 48, "Number of requests")
-	debug = *flag.Bool("debug", false, "Set logger level to debug")
+	url = flag.String("host", "", "Host to request")
+	requests = flag.Int("requests", 48, "Number of requests")
+	debug = flag.Bool("debug", false, "Set logger level to debug")
 	flag.Parse()
 
-	_, err := http.Get(url + "?n=1")
+	_, err := http.Get(*url + "?n=1")
 	if err != nil {
 		slog.Error("Host not valid")
 		os.Exit(1)
 	}
-	if requests > 48 && requests >= 0 {
+	if *requests > 48 && *requests >= 0 {
 		slog.Error("Requests should be less than or equal to 48 and greater than or equal to 0")
 		os.Exit(1)
 	}
-	if debug {
+	if *debug {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 	}
 }
@@ -44,7 +44,7 @@ func init() {
 func worker(wg *sync.WaitGroup, requestsChan <-chan int) {
 	for n := range requestsChan {
 		startTime := time.Now()
-		res, err := http.Get(url + "?n=" + strconv.Itoa(n))
+		res, err := http.Get(*url + "?n=" + strconv.Itoa(n))
 		if err != nil {
 			slog.Debug("REQ", "n", n, "err", err)
 			continue
@@ -61,14 +61,14 @@ func worker(wg *sync.WaitGroup, requestsChan <-chan int) {
 
 func main() {
 	startTime := time.Now()
-	requestsChan := make(chan int, requests)
+	requestsChan := make(chan int, *requests)
 	var wg sync.WaitGroup
 
 	for range runtime.NumCPU() {
 		wg.Add(1)
 		go worker(&wg, requestsChan)
 	}
-	for i := 0; i <= requests; i++ {
+	for i := 0; i <= *requests; i++ {
 		requestsChan <- i
 	}
 	close(requestsChan)
@@ -77,7 +77,7 @@ func main() {
 
 	duration := int(time.Since(startTime).Round(time.Second).Seconds())
 	if duration > 0 {
-		requestsPerSecond := int(requests / duration)
+		requestsPerSecond := int(*requests / duration)
 		slog.Info("Load test completed", "duration", duration, "rps", requestsPerSecond)
 	}
 }
